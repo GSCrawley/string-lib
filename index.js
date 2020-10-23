5819b7808b64592c985fe57ca2b927c17d88c7d6
GSCrawley <gscrawley@gmail.com>
2020-10-23 13:17:44 -0700

    ..

---
+++ index.js
@@ -0,0 +1,13 @@
+
+String.prototype.allCaps = function() {
+    return this.toUpperCase()
+}
+
+String.prototype.capitalizewords = function(){
+    let str = this.split('')
+    
+    str = str.capitalize()
+    while (i < s.len) {
+        if
+    }
+}
