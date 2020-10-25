c8d86feaf322495ce2d8827ee69bece5a8031040
GSCrawley <gscrawley@gmail.com>
2020-10-25 12:37:53 -0700

    challenge 2 done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
-String.prototype.allCaps = function() {
-    return this.toUpperCase()
-}
+// challenge 1
+function capsFirstLtr(str) { 
+    return str.charAt(0).toUpperCase() + str.slice(1); 
+} 
 
-String.prototype.capitalizewords = function(){
-    let str = this.split('')
-    
-    str = str.capitalize()
-    while (i < s.len) {
-        if
-    }
-}
+// challenge 2
+function allCaps(str) { 
+    return str.toUpperCase(); 
+} 
+
+// function capitalizewords(str) {
+//     let str = this.split('')
+//     str = str.capitalize()
+// //     while (i < s.len) {
+// //         if
+// //     }
+// // }
+
+console.log(capsFirstLtr('whats up'))
+console.log(allCaps('it really is a beautiful day today'))
